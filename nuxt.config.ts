3505bb8b43f422ed730ac4e7fa0ed0b5e71310c3
Orane Orveillon <orane.orveillon@outlook.fr>
2026-05-30 13:33:04 +0200

    Add nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    '@nuxthub/core'
+  ],
 
   devtools: {
     enabled: true
