ef6c85bc429a86d7f2de1bc2a3fdb50a04bac30f
Orane0r <orane.orveillon@outlook.fr>
2026-09-05 19:47:46 +0200

    Début layout et image

--- app/pages/catalog/[id].vue
+++ app/pages/catalog/[id].vue
@@ -3,14 +3,26 @@
 </script>
 
 <template>
-  <div>
+  <UContainer class="flex flex-col gap-4">
     <div>fil d'Ariane</div>
-    <div>image (avec bouton 3d)</div>
-    <div>nom</div>
-    <div>description wikipedia + lien wikipedia</div>
+    <div class="flex flex-row gap-4">
+      <div class="flex-1 min-h-50">
+        <!-- TODO bouton 3d -->
+        <NuxtImg
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg?utm_source=en.wikipedia.org&utm_campaign=api&utm_content=thumbnail_unscaled"
+          alt="Moon"
+          fit="contain"
+          class="rounded-xl"
+        />
+      </div>
+      <div class="flex-2 min-h-50">
+        <div>nom</div>
+        <div>description wikipedia + lien wikipedia</div>
+      </div>
+    </div>
     <div>cadre caractéristiques</div>
     <div>cadre profil vs moyenne</div>
     <div>cadre galerie</div>
     <div>objets liés (à voir)</div>
-  </div>
+  </UContainer>
 </template>
